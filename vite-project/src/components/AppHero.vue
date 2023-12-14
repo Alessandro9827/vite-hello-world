b6589724e5afd62e674edfb6cb3bd1029a23e004
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-14 19:36:31 +0100

    header.h2 and main jumbo

---
+++ vite-project/src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div class="jumbo">
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    div.jumbo {
+        height: 300px;
+        width: 100%;
+        color: white;
+    }
+</style>
---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+    },
+}
+</script>
+
+<template>
+    <header>
+        <h2>
+            La mia prima app con Vite!
+        </h2>
+    </header>
+</template>
+
+<style>
+    h2 {
+        color: red;
+    }
+</style>
